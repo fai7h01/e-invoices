7cce9aeac4e69958160b5f67ae9574893f4fa71c
fai7h01 <l.varsimashvil10@gmail.com>
2024-10-03 16:54:45 +0400

    Initial commit

---
+++ src/main/java/com/accounting/einvoices/EInvoicesApplication.java
@@ -0,0 +1,13 @@
+package com.accounting.einvoices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EInvoicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EInvoicesApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/accounting/einvoices/dto/UserDTO.java
@@ -0,0 +1,24 @@
+package com.accounting.einvoices.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private LocationDTO location;
+    private String password;
+    private String confirmPassword;
+    private String phone;
+
+
+}
