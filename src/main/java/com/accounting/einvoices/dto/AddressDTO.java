ee85672007cca139104018537f5634705d3a24a7
fai7h01 <l.varsimashvil10@gmail.com>
2024-10-09 20:03:43 +0400

    user

--- src/main/java/com/accounting/einvoices/dto/AddressDTO.java
+++ src/main/java/com/accounting/einvoices/dto/AddressDTO.java
@@ -12,17 +12,11 @@ import lombok.Setter;
 public class AddressDTO {
 
     private Long id;
-
     private String addressLine1;
-
     private String addressLine2;
-
     private String city;
-
     private String state;
-
     private String country;
-
     private String zipCode;
 
 }
