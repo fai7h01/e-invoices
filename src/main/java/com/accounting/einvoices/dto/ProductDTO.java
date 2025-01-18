c3eb3018c74b563607d18210d6d90f854958e7c0
fai7h01 <l.varsimashvil10@gmail.com>
2025-01-18 20:25:25 +0400

    validation - product

--- src/main/java/com/accounting/einvoices/dto/ProductDTO.java
+++ src/main/java/com/accounting/einvoices/dto/ProductDTO.java
@@ -7,6 +7,8 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
@@ -21,23 +23,23 @@ public class ProductDTO {
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private Long id;
 
+    @NotBlank
     private String name;
-
     private String description;
-
+    @NotNull
     private Integer quantityInStock;
-
+    @NotNull
     private Integer lowLimitAlert;
-
+    @NotNull
     private BigDecimal price;
-
+    @NotNull
     private Currency currency;
-
+    @NotNull
     private LocalDate createdAt;
-
-    private ProductUnit productUnit;
-
+    @NotNull
+    private ProductUnit productUnit = ProductUnit.UNIT;
+    @NotNull
     private ProductStatus status;
-
+    @NotNull
     private CategoryDTO category;
 }
