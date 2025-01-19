c7246f8c4ffc15efd5d31223221bac1b756d201d
fai7h01 <l.varsimashvil10@gmail.com>
2025-01-19 16:45:24 +0400

    regexp

--- src/main/java/com/accounting/einvoices/dto/WooCommerceCredentialsDTO.java
+++ src/main/java/com/accounting/einvoices/dto/WooCommerceCredentialsDTO.java
@@ -4,6 +4,9 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -14,10 +17,16 @@ public class WooCommerceCredentialsDTO {
 
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private Long id;
+    @NotBlank
+    @Pattern(regexp = "^(https?:\\/\\/)?([a-zA-Z0-9-]+\\.)*[a-zA-Z0-9-]+\\.[a-zA-Z]{2,}(:\\d+)?\\/?$\n")
     @JsonProperty("base_url")
     private String baseUrl;
+    @NotBlank
+    @Pattern(regexp = "^ck_[a-f0-9]{40}$")
     @JsonProperty("consumer_key")
     private String consumerKey;
+    @NotBlank
+    @Pattern(regexp = "^cs_[a-f0-9]{40}$")
     @JsonProperty("consumer_secret")
     private String consumerSecret;
 
