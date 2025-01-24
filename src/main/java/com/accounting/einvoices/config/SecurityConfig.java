edaed76b2d179c1e667b6ebd5bf60f4d51f9d54d
fai7h01 <l.varsimashvil10@gmail.com>
2025-01-24 04:40:22 +0400

    security config

---
+++ src/main/java/com/accounting/einvoices/config/SecurityConfig.java
@@ -0,0 +1,95 @@
+package com.accounting.einvoices.config;
+
+import org.keycloak.adapters.KeycloakConfigResolver;
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(jsr250Enabled = true)
+@Profile("prod")
+public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
+
+    @Value("${app.front.base-url}")
+    private String frontEndUrl;
+    @Value("${app.back.base-url}")
+    private String backEndUrl;
+    @Value("${app.assistant.base-url}")
+    private String assistantUrl;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        http.cors().and().authorizeRequests()
+                .antMatchers("/api/v1/register/**").permitAll()
+                .antMatchers("/api/v1/password/**").permitAll()
+                .antMatchers("/api/v1/clientVendor/**").hasAnyRole("Admin", "Manager", "Employee")
+                .antMatchers("/api/v1/category/**").hasAnyRole("Admin", "Manager", "Employee")
+                .antMatchers("/api/v1/company/**").hasAnyRole("Admin", "Manager", "Employee")
+                .antMatchers("/api/v1/dashboard/**").hasAnyRole("Admin", "Manager", "Employee")
+                .antMatchers("/api/v1/product/**").hasAnyRole("Admin", "Manager", "Employee")
+                .antMatchers("/api/v1/invoice/**").hasAnyRole("Admin", "Manager", "Employee")
+                .antMatchers("/api/v1/mailing/**").hasAnyRole("Admin", "Manager", "Employee")
+                .antMatchers("/api/v1/assistant/**").hasAnyRole("Admin", "Manager", "Employee")
+                .anyRequest()
+                .authenticated()
+                .and()
+                .csrf()
+                .disable();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of(frontEndUrl, backEndUrl, assistantUrl));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        KeycloakAuthenticationProvider keycloakAuthenticationProvider =
+                keycloakAuthenticationProvider();
+        keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(
+                new SimpleAuthorityMapper());
+        auth.authenticationProvider(keycloakAuthenticationProvider);
+    }
+
+    @Bean
+    @Override
+    protected SessionAuthenticationStrategy
+    sessionAuthenticationStrategy() {
+        return new RegisterSessionAuthenticationStrategy(
+                new SessionRegistryImpl());
+    }
+
+    @Bean
+    public KeycloakConfigResolver KeycloakConfigResolver() {
+        return new KeycloakSpringBootConfigResolver();
+    }
+
+}
--- src/main/java/com/accounting/einvoices/service/AIReportingService.java
+++ src/main/java/com/accounting/einvoices/service/AIReportingService.java
@@ -5,10 +5,8 @@ import com.accounting.einvoices.dto.ai_analysis.SalesAnalysisDTO;
 
 public interface AIReportingService {
 
-    //get sales analysis
     SalesAnalysisDTO getSalesAnalysis(int year, int startMonth, int endMonth);
 
-    //get invoice analysis
     InvoiceAnalysisDTO getInvoiceAnalysis(int year, int startMonth, int endMonth);
 
     //get product analysis
